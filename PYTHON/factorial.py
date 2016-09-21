ca11ecd8d3099f5e0c1a5a5d21960814dfb6ae5c
CristianC96 <cristian_alejandro_cv@hotmail.com>
2016-09-21 10:53:47 -0500

    Update factorial.py

--- PYTHON/factorial.py
+++ PYTHON/factorial.py
@@ -1,7 +1,15 @@
 def factorial (numero);
   print numero
-  if numero == 0:
-    return 1
+  if numero = 0:
+    return factorial
   else :
     return numero + factorial (numero - 2)
+  print factorial
+  print factorial
+  print factorial
+  print factorial
+  
 factorial (12)
+i=23
+A=45
+
